01230295d175edccc0258c665b65e1bc187794c3
Abhash Rai <avash.cmling@gmail.com>
2023-08-23 12:13:02 +0545

    Add: Created face.py script - basic attendence script

---
+++ face.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def image_encoder(image: np.ndarray) -> np.ndarray:
+    '''
+    Encodes a single given image into 128-dimension face encoding
+    Input -> (image) 1 Dimensional numpy array representation of an image
+    Return -> Face encoding (NumPy array)
+    '''
+    pass
+
+def encode_multiple_images(images: list) -> list:
+    '''
+    Encodes a given list of np.ndarray image encoding into a list of same length list of 128-dimension np.ndarray face encoding for each element in the given list
+    Input -> (images) List of 1 Dimensional numpy array representation of images
+    Return -> list of face encodings
+    '''
+    pass
+
+def compare_with_database_encoding(database_encodings: list, unknown_face_encoding: np.ndarray):
+    '''
+    Compares 'unknown_face_encoding' with list of 'database_encodings'
+    Return -> list of a euclidean distance for each comparison face
+    '''
+    pass
+
+def main():
+    '''
+    Main script to run
+    '''
+    pass
