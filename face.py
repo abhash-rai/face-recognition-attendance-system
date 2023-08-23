a3be607713a5afa7839077d72f69375def3b1923
Abhash Rai <avash.cmling@gmail.com>
2023-08-23 12:19:49 +0545

    Update: Added image_path_to_numpy_array func

--- face.py
+++ face.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+def image_path_to_numpy_array(image_paths: list) -> np.ndarray:
+    '''
+    Converts given 'image_paths' to a list of numpy array representation of those image paths
+    Return -> list (of np.ndarray)
+    '''
+    pass
+
 def image_encoder(image: np.ndarray) -> np.ndarray:
     '''
     Encodes a single given image into 128-dimension face encoding
@@ -14,7 +21,6 @@ def encode_multiple_images(images: list) -> list:
     Input -> (images) List of 1 Dimensional numpy array representation of images
     Return -> list of face encodings
     '''
-    pass
 
 def compare_with_database_encoding(database_encodings: list, unknown_face_encoding: np.ndarray):
     '''
