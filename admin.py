fa54fa25db5ad9c8b9042fc4605d54503c7a53cc
Abhash Rai <avash.cmling@gmail.com>
2023-08-23 14:05:55 +0545

    Update: Added entry from frame capture functionality

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 import face_recognition
-
+import cv2
+import numpy as np
 
 class Admin:
 
@@ -7,16 +8,25 @@ class Admin:
         pass
 
     def image_to_encoding(self, image):
-        face=face_recognition.load_image_file(image)
-        encoding= face_recognition.face_encodings(face)[0]
+        encoding= face_recognition.face_encodings(image)[0]
         return encoding
     
+    def capture_frame(cam_index=0):
+        cap = cv2.VideoCapture(0)
+        if not cap.isOpened():
+            print("Error: Could not open camera.")
+            return
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not capture a frame.")
+            cap.release()
+            return
+        captured_image_array = np.array(frame)
+        cap.release()
+        return captured_image_array
 
-    def make_entry(self, student_id: int, student_name:str, batch:str, method='cam'):
-        if method == 'cam':
-            pass
-            
-        elif method == 'img':
-            pass
+    def make_entry_from_cam(self, student_id: int, student_name:str, batch:str, cam_index=0):
+        captured = self.capture_frame
+        face_encoding = self.image_to_encoding(captured)
             
     
