16949861bddfe8e3291d3fe9ba8411eb55a58d96
Abhash Rai <avash.cmling@gmail.com>
2023-08-26 12:41:58 +0545

    Update: script terminates after sending the data

--- send_face_encodings.py
+++ send_face_encodings.py
@@ -3,33 +3,32 @@ import numpy as np
 from socket import *
 import json
 
-face_endocings_directory_path = './database/face_encodings'
+face_endocings_directory_path = './database/face_encodings/'
 
 def send_json_face_encodings(server_ip_address, server_port, chunksize):
     sock = socket()
     sock.bind((server_ip_address, server_port))
     sock.listen(1)
 
-    while True:
-        print('Waiting for a client...')
-        client, address = sock.accept()
-        print(f'Client joined from {address}')
-        with client:
-            encodings_data = { str(tuple(np.load(face_endocings_directory_path+encoded_face_id))):encoded_face_id.split('.')[0] for encoded_face_id in os.listdir(face_endocings_directory_path) }
-            encodings_json = json.dumps(encodings_data).encode()
+    print('\nWaiting for a client...')
+    client, address = sock.accept()
+    print(f'\nClient joined from {address}')
+    
+    encodings_data = { str(tuple(np.load(face_endocings_directory_path+encoded_face_id))):encoded_face_id.split('.')[0] for encoded_face_id in os.listdir(face_endocings_directory_path) }
+    encodings_json = json.dumps(encodings_data).encode()
 
-            print('Sending data')
+    print('\nSending data...')
 
-            total_bytes = len(encodings_json)
-            num_chunks = (total_bytes + chunksize - 1) // chunksize
+    total_bytes = len(encodings_json)
+    num_chunks = (total_bytes + chunksize - 1) // chunksize
 
-            client.sendall(str(num_chunks).encode() + b'\n')
+    client.sendall(str(num_chunks).encode() + b'\n')
 
-            for i in range(0, total_bytes, chunksize):
-                chunk = encodings_json[i:i + chunksize]
-                client.sendall(chunk)
+    for i in range(0, total_bytes, chunksize):
+        chunk = encodings_json[i:i + chunksize]
+        client.sendall(chunk)
 
-        print('Done.')
+    print('\nDone.')
 
 send_json_face_encodings(
     server_ip_address = '', 
